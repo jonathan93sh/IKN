2aba74b11710bf65083c09e5cd75f8352bc850a7
jonathan93sh <schouga4rd@gmail.com>
2016-02-24 11:01:09 +0100

    v0.0

---
+++ Exercise_8/LIB/bin/Release/lib.d
@@ -0,0 +1,3 @@
+/root/Projects/Exercise_8_c++/LIB/bin/Release/lib.o: \
+ /root/Projects/Exercise_8_c++/LIB/lib.cpp \
+ /root/Projects/Exercise_8_c++/LIB/lib.h
---
+++ Exercise_8/file_server/file_server.cpp
@@ -0,0 +1,51 @@
+//============================================================================
+// Name        : file_server.cpp
+// Author      : Lars Mortensen
+// Version     : 1.0
+// Description : file_server in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <lib.h>
+
+using namespace std;
+
+void sendFile(string fileName, long fileSize, int outToClient);
+
+/**
+ * main starter serveren og venter på en forbindelse fra en klient
+ * Læser filnavn som kommer fra klienten.
+ * Undersøger om filens findes på serveren.
+ * Sender filstørrelsen tilbage til klienten (0 = Filens findes ikke)
+ * Hvis filen findes sendes den nu til klienten
+ *
+ * HUSK at lukke forbindelsen til klienten og filen nÃ¥r denne er sendt til klienten
+ *
+ * @throws IOException
+ *
+ */
+int main(int argc, char *argv[])
+{
+	// TO DO Your own code
+}
+
+/**
+ * Sender filen som har navnet fileName til klienten
+ *
+ * @param fileName Filnavn som skal sendes til klienten
+ * @param fileSize Størrelsen på filen, 0 hvis den ikke findes
+ * @param outToClient Stream som der skrives til socket
+	 */
+void sendFile(string fileName, long fileSize, int outToClient)
+{
+	// TO DO Your own code
+}
+
