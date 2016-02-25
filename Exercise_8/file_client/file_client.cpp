3801dba07ff8400539c0ef16ce95b4d4244f1c32
Laimonas Ignas Bendikas <laimutis.bendikas@gmail.com>
2016-02-25 11:57:56 +0100

    client done

--- Exercise_8/file_client/file_client.cpp
+++ Exercise_8/file_client/file_client.cpp
@@ -23,30 +23,48 @@ void receiveFile(string fileName, int socketfd);
 
 int main(int argc, char *argv[])
 {
-	int socketfd, var;
+	int socketfd, var, serverPort = PORT;
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
 
-    if (argc < 3)
-    	error("Too few input arguments");
+	// syntax ./file_client <file_server’s ip-adr.> <[sti] + filnavn>
+  	if (argc < 2)
+    	error("For faa input argumenter");
+
+  	if(check_File_Exists(extractFileName(argv[1])) != 0)
+  		error("Filen findes allerede");
 
 	sockfd = socket(AF_INET,SOCK_STREAM,0); //if socket fails it returns -1
 	if(sockfd < 0)
-		error("Couldn't open socket");
+	{
+		close(sockfd);
+		error("Kunne ikke oprette socket");
+	}
 
-	server = gethostbyname(argv[1]); //hvis systemet ikke kan finde navnet bliver server til NULL
+	server = gethostbyname(argv[0]); //hvis systemet ikke kan finde navnet bliver server til NULL
 	if (server == NULL)
-		error("There is no such host");
+	{
+		close(sockfd);
+		error("Den angivne host kan ikke findes");
+	}
 
+	//udfylder serv_addr structen med info
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-	serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_port = htons(serverPort);
+
 	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) //0 ved succes -1 ved fejl
-		error("Coulnd't establish connection to server");
-	
-	//write argumenterne her
-	
+	{
+		close(sockfd);
+		error("Kunne ikke oprette forbindelse til serveren");
+	}
+
+	//write to serveren
+	var = writeTextTCP(argv[1], sockfd);
+	if(var < 0)
+		error("Kunne ikke skrive til socketen");
+
   	close(sockfd);
   	return 0;
 }
@@ -62,7 +80,38 @@ int main(int argc, char *argv[])
  * @param sockfd Stream for at skrive til/læse fra serveren
  */
 void receiveFile(string fileName, int sockfd)
-{
+{	
+ 	bool retry = false;
 
-	// TO DO Your own code
+ 	do
+ 	{
+	 	char buf;
+
+	 	long size = getFileSizeTCP(sockfd);
+	 	string md5 = readTextTCP(fileName,sockfd);
+
+	 	ofstream file; 
+	 	file.open(fileName);
+
+	 	for(long i = 0; i<size; i++)
+	 	{
+	 		read(sockfd,&buf, 1);
+	 		file.write(buf);
+	 	}
+
+		file.close();
+		if(md5 != getFile_md5_sum(fileName))
+		{
+			retry = true;
+			string nope = "It's not ok yet";
+			write(sockfd,nope.c_str(), nope.size());
+		}
+		else
+		{
+			retry = false;
+			string ok = "ok";
+			write(sockfd,ok.c_str(), ok.size());
+		}
+
+	} while(retry);
 }
